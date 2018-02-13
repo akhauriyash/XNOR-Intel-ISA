25aa944830ce032775d50683492b6afbe6f9c165
Yash Akhauri <akhauri.yash@gmail.com>
2018-02-13 11:38:00 +0530

    Update include.h

--- include.h
+++ include.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "omp.h"
 #include <stdlib.h>
+#include <math.h>
 
 int f(int x);
 int g(int x);
@@ -10,6 +11,4 @@ void thread_array_add();
 void call_parallel();
 void thread_limiter();
 void print_parallel();
-int f(int x){x++; return x;}
-int g(int x){x = x*x*x; return x;}
-int h(int x){x = x*x; return x;}
+void pi();
