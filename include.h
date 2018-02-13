84680e1a20c2b50da0bea50d787ae4fe5e6437d9
Yash Akhauri <akhauri.yash@gmail.com>
2018-02-13 11:22:42 +0530

    Update include.h

--- include.h
+++ include.h
@@ -9,7 +9,7 @@ void proc_count();
 void thread_array_add();
 void call_parallel();
 void thread_limiter();
-
+void print_parallel();
 int f(int x){x++; return x;}
 int g(int x){x = x*x*x; return x;}
 int h(int x){x = x*x; return x;}
